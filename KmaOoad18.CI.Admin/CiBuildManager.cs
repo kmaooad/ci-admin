85d7d5d5a3d730c677e9943c18c1abe12cd1bb4a
Anton Shabinskiy <a@shbk.me>
2018-09-23 11:38:02 +0300

    Admin CI improvement and polishing * Add secrets config * Enhance summary generation * Make a single procedure for CI setup update

---
+++ KmaOoad18.CI.Admin/CiBuildManager.cs
@@ -0,0 +1,239 @@
+﻿using Microsoft.TeamFoundation.Build.WebApi;
+using Microsoft.TeamFoundation.DistributedTask.WebApi;
+using Microsoft.TeamFoundation.Core.WebApi;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Configuration;
+
+namespace KmaOoad18.CI.Admin
+{
+    public class CiBuildManager
+    {
+        static readonly String vsts_collectionUri = ConfigurationManager.AppSettings[nameof(vsts_collectionUri)];
+        static readonly String vsts_projectName = ConfigurationManager.AppSettings[nameof(vsts_projectName)];
+        static readonly String vsts_pat = ConfigurationManager.AppSettings[nameof(vsts_pat)];
+        static readonly string vsts_projectId = ConfigurationManager.AppSettings[nameof(vsts_projectId)];
+        static readonly string vsts_githubConnection = ConfigurationManager.AppSettings[nameof(vsts_githubConnection)];
+        static readonly string github_pat = ConfigurationManager.AppSettings[nameof(github_pat)];
+
+        private VssConnection _connection;
+        private BuildHttpClient _buildClient;
+        private ConnectedServiceHttpClient _serviceClient;
+        private ProjectHttpClient _projectClient;
+
+        private CiBuildManager()
+        {
+        }
+
+        public static CiBuildManager Create()
+        {
+            var mgr = new CiBuildManager();
+
+            mgr._connection = new VssConnection(new Uri(vsts_collectionUri), new VssBasicCredential(string.Empty, vsts_pat));
+
+            mgr._buildClient = mgr._connection.GetClient<BuildHttpClient>();
+            mgr._serviceClient = mgr._connection.GetClient<ConnectedServiceHttpClient>();
+            mgr._projectClient = mgr._connection.GetClient<ProjectHttpClient>();
+
+            return mgr;
+        }
+
+        public async Task<List<Repository>> GetRepos()
+        {
+            var client = new GitHubClient(new Octokit.ProductHeaderValue("kmaooad18-ci"));
+            var tokenAuth = new Credentials(github_pat);
+            client.Credentials = tokenAuth;
+
+            const string repoNamePattern = @"^(assignment-w\d-.+)$";
+
+            var repos = (await client.Repository.GetAllForOrg("kmaooad18"))
+            .Where(r => Regex.IsMatch(r.Name, repoNamePattern)).ToList();
+
+            return repos;
+        }
+
+        public async Task<string> GetProjectId() =>
+            (await _projectClient.GetProjects()).FirstOrDefault(p => p.Name == vsts_projectName)?.Id.ToString();
+
+        public async Task<BuildDefinitionReference> GetBuildDefinitionReference(string name)
+        => (await _buildClient.GetDefinitionsAsync(project: vsts_projectName)).FirstOrDefault(d => d.Name == name);
+
+        public async Task UploadFile(string repo, string filePath, string content)
+        {
+            var client = new GitHubClient(new Octokit.ProductHeaderValue("kmaooad18-ci"));
+
+            var repoId = (await client.Repository.GetAllForOrg("kmaooad18")).FirstOrDefault(r => r.Name.ToLower() == repo.ToLower())?.Id;
+
+            if (!repoId.HasValue)
+                return;
+
+            var tokenAuth = new Credentials(github_pat);
+            client.Credentials = tokenAuth;
+
+            var file = (await client.Repository.Content.GetAllContents(repoId.Value)).FirstOrDefault(f => f.Path.ToLower() == filePath.ToLower());
+
+            if (file != null)
+                await client.Repository.Content.UpdateFile(repoId.Value, filePath, new UpdateFileRequest($"Update {file.Name}", content, file.Sha, true));
+            else
+                await client.Repository.Content.CreateFile(repoId.Value, filePath, new CreateFileRequest($"Create {filePath}", content, true));
+        }
+
+        public async Task DeleteDefinitions()
+        {
+            var defs = await _buildClient.GetDefinitionsAsync(project: vsts_projectName);
+            foreach (var d in defs)
+            {
+                await _buildClient.DeleteDefinitionAsync(new Guid(vsts_projectId), d.Id);
+            }
+        }
+
+        public async Task SetBuildTrigger()
+        {
+            var defs = await _buildClient.GetDefinitionsAsync(project: vsts_projectName);
+            foreach (var d in defs)
+            {
+                var buildDef = await _buildClient.GetDefinitionAsync(d.Id);
+                buildDef.Triggers.Add(new ScheduleTrigger() { Schedules = new List<Schedule>() { new Schedule() { DaysToBuild = ScheduleDays.All, StartHours = 1, StartMinutes = 0 } } });
+
+                await _buildClient.UpdateDefinitionAsync(buildDef);
+            }
+        }
+
+        public async Task<string> GenerateBuildSummary()
+        {
+            const string defRegex = @"^assignment-(?<week>w\d)-(?<student>.+)-ci$";
+
+            var defs = await _buildClient.GetDefinitionsAsync(project: vsts_projectName);
+
+            var summaryData = new Dictionary<(string Week, string Student), string>();
+
+            foreach (var def in defs)
+            {
+                if (Regex.IsMatch(def.Name, defRegex))
+                {
+                    var defSpecs = Regex.Matches(def.Name, defRegex)[0].Groups;
+
+                    var url = $"https://dev.azure.com/ashabinskiy/KmaOoad18/_build/latest?definitionId={def.Id}";
+
+                    var w = defSpecs["week"].Value;
+                    var s = defSpecs["student"].Value;
+
+                    var defBadge = $" [![Build Status](https://dev.azure.com/ashabinskiy/KmaOoad18/_apis/build/status/assignment-{w}-{s}-ci)]({url}) |";
+
+                    summaryData.Add((w, s), defBadge);
+                }
+            }
+
+            var summary = new StringBuilder();
+
+            var notes = new[] {
+               "- List of students is re-generated nightly",
+               "- If your build shows as never built, make any new commit (or edit any file on GitHub) to trigger CI"
+            };
+
+            foreach (var n in notes)
+                summary.AppendLine(n);
+
+            summary.AppendLine();
+            summary.AppendLine();
+            summary.Append("| Student |");
+
+            var weeks = summaryData.Keys.Select(k => k.Week).Distinct().OrderBy(w => w).ToList();
+            var students = summaryData.Keys.Select(k => k.Student).Distinct().OrderBy(s => s).ToList();
+
+            foreach (var w in weeks)
+            {
+                summary.Append($" {w} |");
+            }
+            summary.AppendLine();
+
+            summary.Append("| --- |");
+
+            foreach (var w in weeks)
+            {
+                summary.Append($" --- |");
+            }
+
+            summary.AppendLine();
+
+
+            foreach (var s in students)
+            {
+                var sl = new StringBuilder($"| {s} |");
+                foreach (var w in weeks)
+                {
+                    if (summaryData.ContainsKey((w, s)))
+                    {
+                        sl.Append(summaryData[(w, s)]);
+                    }
+                    else
+                    {
+                        sl.Append(" N/A |");
+                    }
+                }
+
+                summary.AppendLine(sl.ToString());
+            }
+
+            return summary.ToString();
+        }
+
+        public async Task<bool> CreateDefinition(string repoName, Uri cloneUrl)
+        {
+            var buildDefName = $"{repoName}-ci";
+
+            var existing = await GetBuildDefinitionReference(buildDefName);
+
+            if (existing != null)
+                return false;
+
+            var buildDef = new BuildDefinition();
+            buildDef.Name = buildDefName;
+
+            var yaml = new YamlProcess();
+            yaml.YamlFilename = "/azure-pipelines.yml";
+            buildDef.Process = yaml;
+
+            var queue = new AgentPoolQueue();
+            queue.Id = 34;
+
+            buildDef.Queue = queue;
+
+            var repoNameParts = repoName.Split('-');
+
+            buildDef.Variables.Add("ParentRepository", new BuildDefinitionVariable() { Value = $"{repoNameParts[0]}-{repoNameParts[1]}" });
+
+            var trigger = new ContinuousIntegrationTrigger();
+            trigger.BranchFilters.Add("+master");
+            buildDef.Triggers.Add(trigger);
+
+            var repo = new BuildRepository();
+
+            repo.Type = RepositoryTypes.GitHub;
+            repo.Url = cloneUrl;
+            repo.DefaultBranch = "master";
+            repo.Name = $"kmaooad18/{repoName}";
+            repo.Id = $"kmaooad18/{repoName}";
+            repo.Properties["connectedServiceId"] = vsts_githubConnection;
+
+            buildDef.Repository = repo;
+
+            buildDef = await _buildClient.CreateDefinitionAsync(buildDef, project: vsts_projectName);
+
+            return true;
+        }
+
+    }
+}
---
+++ KmaOoad18.CI.Admin/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmaOoad18.CI.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDefs();
+
+            var a = CiBuildManager.Create();
+            Console.WriteLine("Generating build summary...");
+            var buildSummary = a.GenerateBuildSummary().GetAwaiter().GetResult();
+
+            Console.WriteLine("Uploading build summary...");
+            a.UploadFile("course-home", "builds.md", buildSummary).GetAwaiter().GetResult();
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+
+
+
+        static void GenerateBuildMd()
+        {
+            var a = CiBuildManager.Create();
+            var str = a.GenerateBuildSummary().GetAwaiter().GetResult();
+            using (var f = File.CreateText("builds.md"))
+            {
+                f.Write(str);
+                f.Flush();
+                f.Close();
+            }
+        }
+
+        static void CreateDefs()
+        {
+            var a = CiBuildManager.Create();
+            
+            var repos = (a.GetRepos()).GetAwaiter().GetResult().OrderBy(r => r.Name).ToList();
+            Console.WriteLine($"Found {repos.Count} repos");
+            foreach (var r in repos)
+            {
+                try
+                {
+                    Console.Write($"Creating CI build for {r.Name}....");
+
+                    var result = a.CreateDefinition(r.Name, new Uri(r.CloneUrl)).GetAwaiter().GetResult();
+
+                    Console.WriteLine(result ? "CREATED" : "SKIPPED");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ERROR: {ex.Message}");
+                }
+            }
+        }
+    }
+}
