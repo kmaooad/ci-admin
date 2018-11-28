8176cf3055b8532106caf4aad8d7704014183a14
Anton Shabinskiy <a@shbk.me>
2018-11-28 08:15:36 +0200

    Strip PAT and add multiple files upload

--- FileUploader/UnitTest1.cs
+++ FileUploader/UnitTest1.cs
@@ -1,5 +1,6 @@
 using KmaOoad18.CI.Admin;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Xunit;
@@ -11,23 +12,30 @@ namespace FileUploader
         [Fact]
         public async Task Test1()
         {
-            var pat = "b0b7e192ab96fe41662a817df307a361624c4cfd";
-            var targetFile = @"Leanware.Web/Spec.cs";
-            var sourceFile = @"D:\Projects\_edu\kmaooad18\teacher\assignment-w12\Leanware.Web\Spec.cs";
+            var pat = "5e91a39ae3938439225a68b576b42001fac65c8d";
+
+            var files = new List<(string source, string target)>()
+            {
+                (@"D:\Projects\_edu\kmaooad18\teacher\assignment-w12\Leanware.Web\Spec.cs", @"Leanware.Web/Spec.cs"),
+                (@"D:\Projects\_edu\kmaooad18\teacher\assignment-w12\README.md", @"README.md"),
+            };
 
             var repos = await CiBuildManager.GetRepos(pat, "kmaooad18", @"^(assignment-w12-.+)$");
 
-            string content = null;
-
-            using (var file = new StreamReader(sourceFile))
+            foreach (var (source, target) in files)
             {
-                content = await file.ReadToEndAsync();
-            }
+                string content = null;
 
-            if (content != null)
-            {
-                foreach (var r in repos)
-                    await CiBuildManager.UploadFile(pat, r.Name, targetFile, content);
+                using (var reader = new StreamReader(source))
+                {
+                    content = await reader.ReadToEndAsync();
+                }
+
+                if (content != null)
+                {
+                    foreach (var r in repos)
+                        await CiBuildManager.UploadFile(pat, r.Name, target, content);
+                }
             }
         }
     }
